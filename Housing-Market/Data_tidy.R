b0ca4fd4fd2d7066d7ea8f5ceb32b50a56beaab5
Ali Keller <47513611+alicekeller@users.noreply.github.com>
2022-06-13 15:15:14 -0600

    Update Data_tidy.R

--- Housing-Market/Data_tidy.R
+++ Housing-Market/Data_tidy.R
@@ -3,8 +3,10 @@ library(tidyverse)
 setwd("~/Documents/Data")
 housing <- read_csv("housing_data.csv")
 
+#create new columns for month and year
 housing_tidy <- housing %>%
   separate(`Month of Period End`, into = c("Month", "Year"), convert = TRUE) %>% 
+#change values reading (eg) "10K" to "10,000" for manipulation and visualization
   mutate(`Median Sale Price` = case_when(
     str_detect(`Median Sale Price`, "K") ~ as.numeric(str_extract(`Median Sale Price`, "[\\d\\.]+")) * 1000
   ))
